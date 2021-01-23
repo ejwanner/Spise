2768b5aff06021ddf399c6914f3ee833ba8fbf67
Elias Wanner <elias.wanner.15@gmail.com>
2021-01-23 16:22:03 +0100

    change name material app

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Spise',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
